687db6343590b38b6a2262b63512eb311dc54ccb
Rei Kaizen <bondenie@gmail.com>
2023-05-12 01:48:09 +0800

    include python functions with error handling

--- calculator.py
+++ calculator.py
@@ -4,34 +4,53 @@ def calculator():
     user_choice_operation = input("Choose one of the operations (Addition, Subtraction, Multiplication, and Multiplication): ")  
     ## Addition, Subtraction, Multiplication, and Multiplication
     # ask the user to enter two numbers
-    user_num1 = float(input("Enter the 1st number: "))
-    user_num2 = float(input("Enter the 2nd number: "))
-    # display the result
-    if user_choice_operation == "Addition":
-        print(user_num1 + user_num2)
-    elif user_choice_operation == "Subtraction":
-        print(user_num1 - user_num2)
-    elif user_choice_operation == "Multiplication":
-        print(user_num1 * user_num2)
-    elif user_choice_operation == "Division":
-        print(user_num1 / user_num2)
-    else:
-        print("Invalid Operation")
+    user_num1 = input("Enter the 1st number: ")
+    user_num2 = input("Enter the 2nd number: ")
     
-        
-    # use loop to ask if the user wants to try again or not.
-    while True:
-        user_attempt = input("Do you want to try again or not? (yes/no): ")
-        ## If yes, then perform the operation again.
-        if user_attempt == "yes":
-            calculator()
-        ## if no, Display “Thank you!”
+    
+    # use Python Functions and appropriate Exceptions to capture errors during runtime.
+    try:
+        if (user_num1.isnumeric() and user_num2.isnumeric()):
+            user_num1 = float(user_num1)
+            user_num2 = float(user_num2)  
+            # display the result
+            if user_choice_operation == "Addition":
+                print(user_num1 + user_num2)
+            elif user_choice_operation == "Subtraction":
+                print(user_num1 - user_num2)
+            elif user_choice_operation == "Multiplication":
+                print(user_num1 * user_num2)
+            elif user_choice_operation == "Division":
+                if user_num2 == 0:
+                    raise ZeroDivisionError("Division by zero is undefined :|")
+                print(user_num1 / user_num2)
+            else:
+                raise Exception("Add, Subtract, Multiplication and Division operations only :(")
         else:
-            print("Thank you!")
-            
-        break
-            
-        # then the program will exit
-        #Use Python Functions and appropriate Exceptions to capture errors during runtime.
+            raise ValueError("Please enter a valid numbers :)")
+    
+    except ValueError as raised :
+        print(raised)
+    except ZeroDivisionError as raised:
+        print(raised)
+    except Exception as raised:
+        print(raised)
 
+    finally:
+        print("===" * 3)
+            
+        # use loop to ask if the user wants to try again or not.
+        while True:
+            user_attempt = input("Do you want to try again or not? (yes/no): ")
+            ## If yes, then perform the operation again.
+            if user_attempt == "yes":
+                calculator()
+            ## if no, Display “Thank you!”
+            else:
+                print("Thank you!")
+            
+            break
+                
+            # then the program will exit
+                    
 calculator()
